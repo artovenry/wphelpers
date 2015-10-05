3f0a78c2540548f103eb7562ab6d09bc905874dd
artovenry <artovenry@gmail.com>
2015-10-05 12:54:15 +0900

    絵文字関連のスクリプトをじょきょする関数を追加

--- lib/Artovenry/Wp/functions.php
+++ lib/Artovenry/Wp/functions.php
@@ -17,3 +17,16 @@ function javascript_include_tag($name=""){
   return sprinf($format, $url);
 }
 
+
+function remove_buildin_scripts(){
+  remove_action('wp_head', 'wp_generator');
+  remove_action('wp_head', 'wlwmanifest_link');
+  remove_action('wp_head', 'rsd_link');
+  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+  remove_action( 'wp_print_styles', 'print_emoji_styles' );
+  remove_action( 'admin_print_styles', 'print_emoji_styles' );    
+  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );    
+  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+}
